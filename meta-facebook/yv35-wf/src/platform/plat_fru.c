9e1baa86a6a584fde8f28029921a7c9f4031791e
matt <matt.lin@quantatw.com>
2022-05-19 19:17:39 +0800

    fbyv3.5_wf: Configure Waimono Falls FRU

--- meta-facebook/yv35-wf/src/platform/plat_fru.c
+++ meta-facebook/yv35-wf/src/platform/plat_fru.c
@@ -3,15 +3,13 @@
 #include "plat_fru.h"
 #include "fru.h"
 
-#define RF_FRU_PORT 0x02
-#define RF_FRU_ADDR (0xA8 >> 1)
 
 const EEPROM_CFG plat_fru_config[] = {
 	{
 		NV_ATMEL_24C128,
-		RF_FRU_ID,
-		RF_FRU_PORT,
-		RF_FRU_ADDR,
+		WF_FRU_ID,
+		WF_FRU_PORT,
+		WF_FRU_ADDR,
 		FRU_DEV_ACCESS_BYTE,
 		FRU_START,
 		FRU_SIZE,
--- meta-facebook/yv35-wf/src/platform/plat_fru.h
+++ meta-facebook/yv35-wf/src/platform/plat_fru.h
@@ -1,8 +1,12 @@
 #ifndef PLAT_FRU_H
 #define PLAT_FRU_H
+#include "plat_i2c.h"
+
+#define WF_FRU_PORT I2C_BUS3
+#define WF_FRU_ADDR (0xA8 >> 1)
 
 enum FRU_ID {
-	RF_FRU_ID,
+	WF_FRU_ID,
 	MAX_FRU_ID,
 };
 
