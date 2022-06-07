12b68bb3528562c42479251e5a08c115f2199d9b
Jerry-wiwynn <jerry_c_chen@wiwynn.com>
2022-06-07 01:31:42 -0400

    fby3: update FRU config Summary: Update FRU config by YV3 DL schematic
    Test plan:
    Build pass

--- meta-facebook/yv3-dl/src/platform/plat_fru.h
+++ meta-facebook/yv3-dl/src/platform/plat_fru.h
@@ -1,15 +1,16 @@
 #ifndef PLAT_FRU_H
 #define PLAT_FRU_H
 
-#define MB_FRU_PORT 0x01
+#define MB_FRU_PORT 0x05
 #define MB_FRU_ADDR 0x54
+#define MB_FRU_START 0x3800
 
-#define DPV2_FRU_PORT 0x08
-#define DPV2_FRU_ADDR 0x51
+#define RISER_FRU_PORT 0x04
+#define RISER_FRU_ADDR 0x51
 
 enum {
 	MB_FRU_ID,
-	DPV2_FRU_ID,
+	RISER_FRU_ID,
 	// OTHER_FRU_ID,
 	MAX_FRU_ID,
 };
