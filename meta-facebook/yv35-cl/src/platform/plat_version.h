8fc0d4c9126f5387b544b3db7d7c04e7e9fd0b6b
Ren Chen <ren_chen@wiwynn.com>
2022-08-17 10:18:28 +0800

    I3C Test for Sequoia

--- meta-facebook/yv35-cl/src/platform/plat_version.h
+++ meta-facebook/yv35-cl/src/platform/plat_version.h
@@ -15,7 +15,7 @@
 #define BIC_FW_YEAR_MSB 0x28
 #define BIC_FW_YEAR_LSB 0x28
 #define BIC_FW_WEEK 0x28
-#define BIC_FW_VER 0x28
+#define BIC_FW_VER 0x98
 #define BIC_FW_platform_0 0x63 // char: c
 #define BIC_FW_platform_1 0x6c // char: l
 #define BIC_FW_platform_2 0x00 // char: '\0'
