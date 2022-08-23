ab4bad8d8e700166e8f802fda1bc12fd9c1bef96
KaidanWu <kaidan_wu@wiwynn.com>
2022-08-23 16:39:19 +0800

    test: for Jeff reference

--- common/hal/hal_i3c.c
+++ common/hal/hal_i3c.c
@@ -4,68 +4,16 @@
 #include <string.h>
 #include <stdlib.h>
 #include <zephyr.h>
+#include <logging/log.h>
 #include "libutil.h"
 
+LOG_MODULE_REGISTER(hal_i3c);
+
 static const struct device *dev_i3c[I3C_MAX_DESC_NUM];
-static uint8_t data_buffer[I3C_MAX_XFER_NUM][I3C_MAX_BUFF_SIZE];
+static const struct device *dev_i3c_smq[I3C_MAX_DESC_NUM];
+static uint8_t data_buf[I3C_MAX_XFER_NUM][I3C_MAX_BUFF_SIZE];
 static struct i3c_dev_desc i3c_desc_table[I3C_MAX_DESC_NUM];
 struct k_mutex i3c_mutex[I3C_MAX_DESC_NUM];
-
-void util_init_i3c(void)
-{
-	int status;
-
-#ifdef DEV_I3C_0
-	dev_i3c[0] = device_get_binding("I3C_0");
-	status = k_mutex_init(&i3c_mutex[0]);
-	if (status)
-		printf("i3c0 mutex init fail\n");
-#endif
-#ifdef DEV_I3C_1
-	dev_i3c[1] = device_get_binding("I3C_1");
-	status = k_mutex_init(&i3c_mutex[1]);
-	if (status)
-		printf("i3c1 mutex init fail\n");
-#endif
-#ifdef DEV_I3C_2
-	dev_i3c[2] = device_get_binding("I3C_2");
-	status = k_mutex_init(&i3c_mutex[2]);
-	if (status)
-		printf("i2c2 mutex init fail\n");
-#endif
-#ifdef DEV_I3C_3
-	dev_i3c[3] = device_get_binding("I3C_3");
-	status = k_mutex_init(&i3c_mutex[3]);
-	if (status)
-		printf("i2c3 mutex init fail\n");
-#endif
-#ifdef DEV_I3C_4
-	dev_i3c[4] = device_get_binding("I3C_4");
-	status = k_mutex_init(&i3c_mutex[4]);
-	if (status)
-		printf("i3c4 mutex init fail\n");
-#endif
-#ifdef DEV_I3C_5
-	dev_i3c[5] = device_get_binding("I3C_5");
-	status = k_mutex_init(&i3c_mutex[5]);
-	if (status)
-		printf("i3c5 mutex init fail\n");
-#endif
-#ifdef DEV_I3C_6
-	dev_i3c[6] = device_get_binding("I3C_6");
-	status = k_mutex_init(&i3c_mutex[6]);
-	if (status)
-		printf("i3c6 mutex init fail\n");
-#endif
-#ifdef DEV_I3C_7
-	dev_i3c[7] = device_get_binding("I3C_7");
-	status = k_mutex_init(&i3c_mutex[7]);
-	if (status)
-		printf("i3c7 mutex init fail\n");
-#endif
-
-	ARG_UNUSED(status);
-}
 
 static struct i3c_dev_desc *find_matching_desc(const struct device *dev, uint8_t desc_addr)
 {
@@ -97,7 +45,7 @@ int i3c_priv_xfer(I3C_MSG *msg, uint8_t retry)
 
 	/* Collect necessary data */
 	dev = dev_i3c[msg->bus];
-	addr = msg->target_addr;
+	//addr = msg->target_addr;
 	desc = find_matching_desc(dev, addr);
 
 	/* Examine data */
@@ -111,13 +59,13 @@ int i3c_priv_xfer(I3C_MSG *msg, uint8_t retry)
 		if (!i) {
 			/* Data for write */
 			msg->xfer[i].rnw = 0;
-			msg->xfer[i].data.out = data_buffer[i];
+			msg->xfer[i].data.out = data_buf[i];
 			msg->xfer[i].len = msg->tx_len;
-			memcpy(&data_buffer[0], &msg->data[0], msg->tx_len);
+			memcpy(&data_buf[0], &msg->data[0], msg->tx_len);
 		} else {
 			/* Data for read */
 			msg->xfer[i].rnw = 1;
-			msg->xfer[i].data.in = data_buffer[i];
+			msg->xfer[i].data.in = data_buf[i];
 			msg->xfer[i].len = msg->rx_len;
 		}
 	}
@@ -141,4 +89,137 @@ int i3c_priv_xfer(I3C_MSG *msg, uint8_t retry)
 	}
 
 	return ret;
+}
+
+
+int i3c_slave_mqueue_read(const struct device *dev, uint8_t *dest, int budget);
+int i3c_slave_mqueue_write(const struct device *dev, uint8_t *src, int size);
+
+int i3c_smq_read() //(I3C_MSG *msg)
+{
+
+	I3C_MSG *msg = (I3C_MSG *)malloc(sizeof(I3C_MSG)); 
+	
+	if (!msg) {
+		printf("[%s] null msg\n", __func__);
+		return -EINVAL;
+	}
+	msg->bus = 0;
+	msg->rx_len = 16;
+
+	int ret = 0;
+	printf("[%s] starting slave read\n", __func__);
+	i3c_slave_mqueue_read(dev_i3c[msg->bus], data_buf[0], msg->rx_len);
+	printf("[%s] complete slave read\n", __func__);
+	printf("[%s] starting data_buf print\n", __func__);	
+	for (int i = 0; i < I3C_MAX_XFER_NUM; ++i) {
+		for (int j = 0; j < I3C_MAX_BUFF_SIZE; ++j) {
+			printf("0x%02x ", data_buf[i][j]);
+		}
+		printf("\n");
+	}
+	printf("[%s] complete data_buf print\n", __func__);	
+	return ret;
+}
+
+int i3c_smq_write(I3C_MSG *msg)
+{
+	int ret;
+	//int retry = 5;
+	if (!msg) {
+		printf("[%s] null msg\n", __func__);
+		return -EINVAL;
+	}
+
+	if (!dev_i3c[msg->bus]) {
+		printf("[%s] bus%u not defined\n", __func__, msg->bus);
+		return -ENODEV;
+	}
+
+	if (I3C_DEBUG) {
+		LOG_HEXDUMP_DBG(msg->data, msg->tx_len, "I3C_SMQ_WRITE");
+	}
+
+	ret = k_mutex_lock(&i3c_mutex[msg->bus], K_SECONDS(1));
+	if (ret) {
+		printf("[%s] k_mutex bus%u lock failed, ret: %d\n", __func__, msg->bus, ret);
+		return ret;
+	}
+
+	/* TODO: if tx_len > data_buf size */
+
+	memcpy(&data_buf[0], &msg->data[0], msg->tx_len);
+	ret = i3c_slave_mqueue_write(dev_i3c[msg->bus], data_buf[0], msg->tx_len);
+	k_mutex_unlock(&i3c_mutex[msg->bus]);
+	printf("[%s] smq write sucessfully\n", __func__);
+	return ret;
+}
+
+void util_init_i3c(void)
+{
+#ifdef DEV_I3C_0
+	dev_i3c[0] = device_get_binding("I3C_0");
+	if (k_mutex_init(&i3c_mutex[0]))
+		printf("i3c0 mutex init fail\n");
+#endif
+#ifdef DEV_I3C_1
+	dev_i3c[1] = device_get_binding("I3C_1");
+	if (k_mutex_init(&i3c_mutex[1]))
+		printf("i3c1 mutex init fail\n");
+#endif
+#ifdef DEV_I3C_2
+	dev_i3c[2] = device_get_binding("I3C_2");
+	if (k_mutex_init(&i3c_mutex[2]))
+		printf("i2c2 mutex init fail\n");
+#endif
+#ifdef DEV_I3C_3
+	dev_i3c[3] = device_get_binding("I3C_3");
+	if (k_mutex_init(&i3c_mutex[3]))
+		printf("i2c3 mutex init fail\n");
+#endif
+#ifdef DEV_I3C_4
+	dev_i3c[4] = device_get_binding("I3C_4");
+	if (k_mutex_init(&i3c_mutex[4]))
+		printf("i3c4 mutex init fail\n");
+#endif
+#ifdef DEV_I3C_5
+	dev_i3c[5] = device_get_binding("I3C_5");
+	if (k_mutex_init(&i3c_mutex[5]))
+		printf("i3c5 mutex init fail\n");
+#endif
+#ifdef DEV_I3C_6
+	dev_i3c[6] = device_get_binding("I3C_6");
+	if (k_mutex_init(&i3c_mutex[6]))
+		printf("i3c6 mutex init fail\n");
+#endif
+#ifdef DEV_I3C_7
+	dev_i3c[7] = device_get_binding("I3C_7");
+	if (k_mutex_init(&i3c_mutex[7]))
+		printf("i3c7 mutex init fail\n");
+#endif
+
+#ifdef DEV_I3CSMQ_0
+	dev_i3c_smq[0] = device_get_binding("I3C_SMQ_0");
+#endif
+#ifdef DEV_I3CSMQ_1
+	dev_i3c_smq[1] = device_get_binding("I3C_SMQ_1");
+#endif
+#ifdef DEV_I3CSMQ_2
+	dev_i3c_smq[2] = device_get_binding("I3C_SMQ_2");
+#endif
+#ifdef DEV_I3CSMQ_3
+	dev_i3c_smq[3] = device_get_binding("I3C_SMQ_3");
+#endif
+#ifdef DEV_I3CSMQ_4
+	dev_i3c_smq[4] = device_get_binding("I3C_SMQ_4");
+#endif
+#ifdef DEV_I3CSMQ_5
+	dev_i3c_smq[5] = device_get_binding("I3C_SMQ_5");
+#endif
+#ifdef DEV_I3CSMQ_6
+	dev_i3c_smq[6] = device_get_binding("I3C_SMQ_6");
+#endif
+#ifdef DEV_I3CSMQ_7
+	dev_i3c_smq[7] = device_get_binding("I3C_SMQ_7");
+#endif
 }
