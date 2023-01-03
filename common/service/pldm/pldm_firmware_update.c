fbf6b9eae8ae55f05e72dc003fd96d1f2f2f62e9
SaraLin-wiwynn <Sara_SY_Lin@wiwynn.com>
2023-01-03 17:12:10 +0800

    fby35: cl: Support MCTP/PLDM to IPMB bridge command
    Summary:
    - Support BIC send MCTP/PLDM request to BMC.
    - Support bridge command from BMC to ME/1OU BIC (BMC -> MCTP/PLDM -> BIC -> IPMB -> KCS/ME/1OU BIC)
    - Support KCS/ME request to BMC (KCS/ME -> IPMB -> BIC -> MCTP/PLDM -> BMC).
    - Support pldm instance id field
    
    Test plan:
    - Build code: Pass
    - Using bridge command: Pass
    - Host power on: Pass
    - Using IPMI command on host: Pass

--- common/service/pldm/pldm_firmware_update.c
+++ common/service/pldm/pldm_firmware_update.c
@@ -404,8 +404,8 @@ exit:
 	return;
 }
 
-static uint8_t request_update(void *mctp_inst, uint8_t *buf, uint16_t len, uint8_t *resp,
-			      uint16_t *resp_len, void *ext_params)
+static uint8_t request_update(void *mctp_inst, uint8_t *buf, uint16_t len, uint8_t instance_id,
+			      uint8_t *resp, uint16_t *resp_len, void *ext_params)
 {
 	if (!mctp_inst || !buf || !resp || !resp_len || !ext_params) {
 		LOG_ERR("Pass argument is NULL");
@@ -459,8 +459,9 @@ exit:
 	return PLDM_SUCCESS;
 }
 
-static uint8_t pass_component_table(void *mctp_inst, uint8_t *buf, uint16_t len, uint8_t *resp,
-				    uint16_t *resp_len, void *ext_params)
+static uint8_t pass_component_table(void *mctp_inst, uint8_t *buf, uint16_t len,
+				    uint8_t instance_id, uint8_t *resp, uint16_t *resp_len,
+				    void *ext_params)
 {
 	if (!mctp_inst || !buf || !resp || !resp_len || !ext_params) {
 		LOG_ERR("Pass argument is NULL");
@@ -534,8 +535,8 @@ exit:
 	return PLDM_SUCCESS;
 }
 
-static uint8_t update_component(void *mctp_inst, uint8_t *buf, uint16_t len, uint8_t *resp,
-				uint16_t *resp_len, void *ext_params)
+static uint8_t update_component(void *mctp_inst, uint8_t *buf, uint16_t len, uint8_t instance_id,
+				uint8_t *resp, uint16_t *resp_len, void *ext_params)
 {
 	if (!mctp_inst || !buf || !resp || !resp_len || !ext_params) {
 		LOG_ERR("Pass argument is NULL");
@@ -633,8 +634,8 @@ exit:
 	return PLDM_SUCCESS;
 }
 
-static uint8_t activate_firmware(void *mctp_inst, uint8_t *buf, uint16_t len, uint8_t *resp,
-				 uint16_t *resp_len, void *ext_params)
+static uint8_t activate_firmware(void *mctp_inst, uint8_t *buf, uint16_t len, uint8_t instance_id,
+				 uint8_t *resp, uint16_t *resp_len, void *ext_params)
 {
 	if (!mctp_inst || !buf || !resp || !resp_len || !ext_params) {
 		LOG_ERR("Pass argument is NULL");
