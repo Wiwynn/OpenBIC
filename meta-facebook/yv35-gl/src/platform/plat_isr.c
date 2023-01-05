95d23cd97650c448f8b5de4f60cd198b542a4d50
Ren Chen <Ren_Chen@wiwynn.com>
2023-01-05 15:45:32 +0800

    fby35: gl: Configure GPIO interrupt function Summary: - BIC accesses the interrupt function when specific GPIOs rising/falling.
    Test Plan:
    - Build code : Pass
    
    Test Log:
        ```
        [302/302] Linking C executable zephyr/Y35BGL.elf
        Memory region         Used Size  Region Size  %age Used
                SRAM_NC:        152 KB       320 KB     47.50%
                FLASH:          0 GB         0 GB
                    SRAM:      431816 B       448 KB     94.13%
                IDT_LIST:          0 GB         2 KB      0.00%

---
+++ meta-facebook/yv35-gl/src/platform/plat_isr.c
@@ -0,0 +1,569 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <logging/log.h>
+
+#include "hal_gpio.h"
+#include "libipmi.h"
+#include "snoop.h"
+#include "ipmi.h"
+#include "power_status.h"
+#include "kcs.h"
+
+#include "plat_isr.h"
+#include "plat_gpio.h"
+#include "plat_ipmb.h"
+#include "plat_sensor_table.h"
+
+LOG_MODULE_REGISTER(plat_isr);
+
+uint8_t _1ou_m2_mapping_table[4] = { 4, 3, 2, 1 };
+uint8_t _1ou_m2_name_mapping_table[4] = {
+	0x3A,
+	0x3C,
+	0x3E,
+	0x37,
+};
+
+void send_gpio_interrupt(uint8_t gpio_num)
+{
+	ipmb_error status;
+	ipmi_msg msg;
+	uint8_t gpio_val;
+
+	gpio_val = gpio_get(gpio_num);
+	LOG_INF("Send gpio interrupt to BMC, gpio number(%d) status(%d)\n", gpio_num, gpio_val);
+
+	msg.data_len = 5;
+	msg.InF_source = SELF;
+	msg.InF_target = BMC_IPMB;
+	msg.netfn = NETFN_OEM_1S_REQ;
+	msg.cmd = CMD_OEM_1S_SEND_INTERRUPT_TO_BMC;
+
+	msg.data[0] = IANA_ID & 0xFF;
+	msg.data[1] = (IANA_ID >> 8) & 0xFF;
+	msg.data[2] = (IANA_ID >> 16) & 0xFF;
+	msg.data[3] = gpio_num;
+	msg.data[4] = gpio_val;
+
+	status = ipmb_read(&msg, IPMB_inf_index_map[msg.InF_target]);
+	if (status != IPMB_ERROR_SUCCESS) {
+		LOG_ERR("Failed to send GPIO interrupt event to BMC, gpio number(%d) status(%d)",
+			gpio_num, status);
+	}
+}
+
+int get_set_1ou_m2_power(ipmi_msg *msg, uint8_t device_id, uint8_t option)
+{
+	CHECK_NULL_ARG_WITH_RETURN(msg, -1);
+	uint32_t iana = IANA_ID;
+	ipmb_error status;
+
+	memset(msg, 0, sizeof(ipmi_msg));
+	msg->InF_source = SELF;
+	msg->InF_target = EXP1_IPMB;
+	msg->netfn = NETFN_OEM_1S_REQ;
+	msg->cmd = CMD_OEM_1S_GET_SET_M2;
+	msg->data_len = 5;
+	memcpy(&msg->data[0], (uint8_t *)&iana, 3);
+	msg->data[3] = _1ou_m2_mapping_table[device_id];
+	msg->data[4] = option;
+	status = ipmb_read(msg, IPMB_inf_index_map[msg->InF_target]);
+	if (status != IPMB_ERROR_SUCCESS) {
+		LOG_ERR("Failed to set get 1OU E1.S power: status 0x%x, id %d, option 0x%x", status,
+			device_id, option);
+		return -1;
+	}
+
+	return 0;
+}
+
+static void SLP3_handler()
+{
+	common_addsel_msg_t sel_msg;
+	if (gpio_get(PWRGD_CPU_LVC3) == GPIO_LOW) {
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_SYS_STA;
+		sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+		sel_msg.sensor_number = SENSOR_NUM_SYSTEM_STATUS;
+		sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_SYS_VRWATCHDOG;
+		sel_msg.event_data2 = 0xFF;
+		sel_msg.event_data3 = 0xFF;
+		if (!common_add_sel_evt_record(&sel_msg)) {
+			LOG_ERR("VR watchdog timeout addsel fail");
+		}
+	}
+}
+
+K_WORK_DELAYABLE_DEFINE(SLP3_work, SLP3_handler);
+void ISR_SLP3()
+{
+	if (gpio_get(PWRGD_CPU_LVC3) == GPIO_LOW) {
+		LOG_INF("slp3\n");
+		k_work_schedule(&SLP3_work, K_MSEC(10000));
+		return;
+	}
+	if (k_work_cancel_delayable(&SLP3_work) != 0) {
+		LOG_ERR("[%s] Failed to cancel delayable work", __func__);
+	}
+}
+
+K_WORK_DELAYABLE_DEFINE(set_DC_on_5s_work, set_DC_on_delayed_status);
+K_WORK_DELAYABLE_DEFINE(set_DC_off_10s_work, set_DC_off_delayed_status);
+/*
+ * TODO :
+ * Implement "Read PMIC error when DC off" when I3C is ready.
+ */
+
+void Set_DC_status()
+{
+	set_DC_status(PWRGD_CPU_LVC3);
+
+	if (get_DC_status() == true) {
+		k_work_schedule(&set_DC_on_5s_work, K_SECONDS(DC_ON_5_SECOND));
+
+		if (k_work_cancel_delayable(&set_DC_off_10s_work) != 0) {
+			LOG_ERR("Cancel set dc off delay work fail");
+		}
+		set_DC_off_delayed_status();
+	} else {
+		set_DC_on_delayed_status();
+		k_work_schedule(&set_DC_off_10s_work, K_SECONDS(DC_OFF_10_SECOND));
+
+		/*
+		 * TODO :
+		 * Implement "Read PMIC error when DC off" when I3C is ready.
+		 */
+
+		if ((gpio_get(PWRGD_CPU_LVC3) == GPIO_HIGH) &&
+		    (gpio_get(PWRGD_AUX_PWRGD_BMC_LVC3) == GPIO_HIGH)) {
+			common_addsel_msg_t sel_msg;
+			sel_msg.InF_target = BMC_IPMB;
+			sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_OEM_C3;
+			sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+			sel_msg.sensor_number = SENSOR_NUM_POWER_ERROR;
+			sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_SYS_PWROK_FAIL;
+			sel_msg.event_data2 = 0xFF;
+			sel_msg.event_data3 = 0xFF;
+			if (!common_add_sel_evt_record(&sel_msg)) {
+				LOG_ERR("System PWROK failure addsel fail");
+			}
+		}
+	}
+}
+
+static void PROC_FAIL_handler(struct k_work *work)
+{
+	/* if have not received kcs and post code, add FRB3 event log. */
+	if ((get_kcs_ok() == false) && (get_postcode_ok() == false)) {
+		common_addsel_msg_t sel_msg;
+		bool ret = false;
+
+		memset(&sel_msg, 0, sizeof(common_addsel_msg_t));
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.sensor_type = IPMI_SENSOR_TYPE_PROCESSOR;
+		sel_msg.sensor_number = SENSOR_NUM_PROC_FAIL;
+		sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+		sel_msg.event_data1 = IPMI_EVENT_OFFSET_PROCESSOR_FRB3;
+		sel_msg.event_data2 = 0xFF;
+		sel_msg.event_data3 = 0xFF;
+		ret = common_add_sel_evt_record(&sel_msg);
+		if (!ret) {
+			LOG_ERR("Fail to assert FRB3 event log.");
+		}
+	}
+}
+
+K_WORK_DELAYABLE_DEFINE(PROC_FAIL_work, PROC_FAIL_handler);
+
+void Initialize_CPU()
+{
+	//check CPU's status by PWRGD_CPU_LVC3
+	set_CPU_power_status(PWRGD_CPU_LVC3);
+	if (gpio_get(PWRGD_CPU_LVC3) == GPIO_HIGH) {
+		init_snoop_thread();
+		init_send_postcode_thread();
+		/* start thread proc_fail_handler after 10 seconds */
+		k_work_schedule(&PROC_FAIL_work, K_SECONDS(PROC_FAIL_START_DELAY_SECOND));
+	} else {
+		abort_snoop_thread();
+
+		if (k_work_cancel_delayable(&PROC_FAIL_work) != 0) {
+			LOG_ERR("Cancel proc_fail delay work fail");
+		}
+
+		reset_kcs_ok();
+		reset_postcode_ok();
+	}
+	send_gpio_interrupt(PWRGD_CPU_LVC3);
+}
+
+void PWRGD_CPU_ACTIVE_HANDLE()
+{
+	ISR_SLP3();
+	Set_DC_status();
+	Initialize_CPU();
+}
+
+void ISR_BMC_PRDY()
+{
+	send_gpio_interrupt(H_BMC_PRDY_BUF_N);
+}
+
+static void CAT_ERR_handler(struct k_work *work)
+{
+	if ((gpio_get(RST_PLTRST_BUF_N) == GPIO_HIGH) || (gpio_get(PWRGD_CPU_LVC3) == GPIO_HIGH)) {
+		common_addsel_msg_t sel_msg;
+		bool ret = false;
+
+		memset(&sel_msg, 0, sizeof(common_addsel_msg_t));
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.sensor_type = IPMI_SENSOR_TYPE_PROCESSOR;
+		sel_msg.sensor_number = SENSOR_NUM_CATERR;
+		sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+		/* MCERR: one pulse, IERR: keep low */
+		if (gpio_get(FM_CATERR_LVT3_N) == GPIO_HIGH) {
+			sel_msg.event_data1 = IPMI_EVENT_OFFSET_PROCESSOR_MCERR;
+		} else {
+			sel_msg.event_data1 = IPMI_EVENT_OFFSET_PROCESSOR_IERR;
+		}
+		sel_msg.event_data2 = 0xFF;
+		sel_msg.event_data3 = 0xFF;
+		ret = common_add_sel_evt_record(&sel_msg);
+		if (!ret) {
+			LOG_ERR("Fail to assert CatErr event log.");
+		}
+	}
+}
+
+K_WORK_DELAYABLE_DEFINE(CAT_ERR_work, CAT_ERR_handler);
+
+void ISR_CATERR()
+{
+	if (gpio_get(RST_PLTRST_BUF_N) == GPIO_LOW) {
+		if (k_work_cancel_delayable(&CAT_ERR_work) != 0) {
+			LOG_ERR("Cancel caterr delay work fail");
+		}
+		/* start thread CatErr_handler after 2 seconds */
+		k_work_schedule(&CAT_ERR_work, K_SECONDS(CATERR_START_DELAY_SECOND));
+	}
+}
+
+void ISR_PLTRST()
+{
+	send_gpio_interrupt(RST_PLTRST_BUF_N);
+}
+
+void ISR_DBP_PRSNT()
+{
+	send_gpio_interrupt(FM_DBP_PRESENT_N);
+}
+
+void ISR_HSC_THROTTLE()
+{
+	common_addsel_msg_t sel_msg;
+	// Flag for filter out fake alert
+	static bool is_hsc_throttle_assert = false;
+	if (gpio_get(PWRGD_AUX_PWRGD_BMC_LVC3) == GPIO_HIGH) {
+		if (gpio_get(PWRGD_CPU_LVC3) == GPIO_HIGH) {
+			return;
+		} else {
+			if ((gpio_get(IRQ_SML1_PMBUS_ALERT_N) == GPIO_HIGH) &&
+			    (is_hsc_throttle_assert == true)) {
+				sel_msg.event_type = IPMI_OEM_EVENT_TYPE_DEASSERT;
+				is_hsc_throttle_assert = false;
+			} else if ((gpio_get(IRQ_SML1_PMBUS_ALERT_N) == GPIO_LOW) &&
+				   (is_hsc_throttle_assert == false)) {
+				sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+				is_hsc_throttle_assert = true;
+			} else { // Fake alert
+				return;
+			}
+
+			sel_msg.InF_target = BMC_IPMB;
+			sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_SYS_STA;
+			sel_msg.sensor_number = SENSOR_NUM_SYSTEM_STATUS;
+			sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_SYS_PMBUSALERT;
+			sel_msg.event_data2 = 0xFF;
+			sel_msg.event_data3 = 0xFF;
+			if (!common_add_sel_evt_record(&sel_msg)) {
+				LOG_ERR("HSC Throttle addsel fail");
+			}
+		}
+	}
+}
+
+static void mb_throttle_handler(struct k_work *work)
+{
+	static bool is_mb_throttle_assert = false;
+	common_addsel_msg_t sel_msg;
+
+	if (((gpio_get(FAST_PROCHOT_N) == LOW_ACTIVE) && (is_mb_throttle_assert == false)) ||
+	    ((gpio_get(FAST_PROCHOT_N) == LOW_INACTIVE) && (is_mb_throttle_assert == true))) {
+		memset(&sel_msg, 0, sizeof(common_addsel_msg_t));
+		if (gpio_get(FAST_PROCHOT_N) == LOW_INACTIVE) {
+			sel_msg.event_type = IPMI_OEM_EVENT_TYPE_DEASSERT;
+			is_mb_throttle_assert = false;
+		} else {
+			sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+			is_mb_throttle_assert = true;
+		}
+
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_SYS_STA;
+		sel_msg.sensor_number = SENSOR_NUM_SYSTEM_STATUS;
+		sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_SYS_FIRMWAREASSERT;
+		sel_msg.event_data2 = 0xFF;
+		sel_msg.event_data3 = 0xFF;
+		if (!common_add_sel_evt_record(&sel_msg)) {
+			LOG_ERR("MB Throttle addsel fail");
+		}
+	}
+}
+
+K_WORK_DELAYABLE_DEFINE(mb_throttle_work, mb_throttle_handler);
+
+void ISR_MB_THROTTLE()
+{
+	if (gpio_get(PWRGD_AUX_PWRGD_BMC_LVC3) == GPIO_HIGH) {
+		if (k_work_cancel_delayable(&mb_throttle_work) != 0) {
+			LOG_ERR("Cancel caterr delay work fail");
+		}
+		/* start thread mb_throttle_handler after 4us */
+		k_work_schedule(&mb_throttle_work, K_USEC(MB_THROTTLE_DELAY_US));
+	}
+}
+
+void ISR_SOC_THMALTRIP()
+{
+	common_addsel_msg_t sel_msg;
+	if (gpio_get(RST_PLTRST_SYNC_LVC3_N) == GPIO_LOW) {
+		if (gpio_get(H_CPU_MEMTRIP_LVC3_N) ==
+		    GPIO_LOW) { // Reference pin for memory thermal trip event
+			sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_SYS_THERMAL_TRIP;
+		} else {
+			sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_SYS_MEMORY_THERMALTRIP;
+		}
+
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+		sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_SYS_STA;
+		sel_msg.sensor_number = SENSOR_NUM_SYSTEM_STATUS;
+		sel_msg.event_data2 = 0xFF;
+		sel_msg.event_data3 = 0xFF;
+		if (!common_add_sel_evt_record(&sel_msg)) {
+			if (sel_msg.event_data1 == IPMI_OEM_EVENT_OFFSET_SYS_THERMAL_TRIP) {
+				LOG_ERR("Failed to add SOC Thermal trip SEL");
+			} else {
+				LOG_ERR("Failed to add Memory Thermal trip SEL");
+			}
+		}
+	}
+}
+
+void ISR_SYS_THROTTLE()
+{
+	common_addsel_msg_t sel_msg;
+	if ((gpio_get(RST_PLTRST_SYNC_LVC3_N) == GPIO_LOW) &&
+	    (gpio_get(PWRGD_CPU_LVC3) == GPIO_HIGH)) {
+		if (gpio_get(FM_CPU_BIC_PROCHOT_LVT3_N) == GPIO_HIGH) {
+			sel_msg.event_type = IPMI_OEM_EVENT_TYPE_DEASSERT;
+		} else {
+			sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+		}
+
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_SYS_STA;
+		sel_msg.sensor_number = SENSOR_NUM_SYSTEM_STATUS;
+		sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_SYS_THROTTLE;
+		sel_msg.event_data2 = 0xFF;
+		sel_msg.event_data3 = 0xFF;
+		if (!common_add_sel_evt_record(&sel_msg)) {
+			LOG_ERR("Failed to add system Throttle SEL");
+		}
+	}
+}
+
+void ISR_HSC_OC()
+{
+	common_addsel_msg_t sel_msg;
+	if (gpio_get(PWRGD_AUX_PWRGD_BMC_LVC3) == GPIO_HIGH) {
+		if (gpio_get(FM_HSC_TIMER) == GPIO_HIGH) {
+			sel_msg.event_type = IPMI_OEM_EVENT_TYPE_DEASSERT;
+		} else {
+			sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+		}
+
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_SYS_STA;
+		sel_msg.sensor_number = SENSOR_NUM_SYSTEM_STATUS;
+		sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_SYS_HSCTIMER;
+		sel_msg.event_data2 = 0xFF;
+		sel_msg.event_data3 = 0xFF;
+		if (!common_add_sel_evt_record(&sel_msg)) {
+			LOG_ERR("Failed to add HSC OC SEL");
+		}
+	}
+}
+
+void ISR_CPU_MEMHOT()
+{
+	common_addsel_msg_t sel_msg;
+	if ((gpio_get(RST_PLTRST_SYNC_LVC3_N) == GPIO_LOW) &&
+	    (gpio_get(PWRGD_CPU_LVC3) == GPIO_HIGH)) {
+		if (gpio_get(H_CPU_MEMHOT_OUT_LVC3_N) == GPIO_HIGH) {
+			sel_msg.event_type = IPMI_OEM_EVENT_TYPE_DEASSERT;
+		} else {
+			sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+		}
+
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_CPU_DIMM_HOT;
+		sel_msg.sensor_number = SENSOR_NUM_CPUDIMM_HOT;
+		sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_DIMM_HOT;
+		sel_msg.event_data2 = 0xFF;
+		sel_msg.event_data3 = 0xFF;
+		if (!common_add_sel_evt_record(&sel_msg)) {
+			LOG_ERR("Failed to add CPU MEM HOT SEL");
+		}
+	}
+}
+
+void ISR_CPUVR_HOT()
+{
+	common_addsel_msg_t sel_msg;
+	if ((gpio_get(RST_PLTRST_SYNC_LVC3_N) == GPIO_HIGH) &&
+	    (gpio_get(PWRGD_CPU_LVC3) == GPIO_HIGH)) {
+		if ((gpio_get(IRQ_PVCCIN_CPU0_VRHOT_N) == GPIO_HIGH) ||
+		    (gpio_get(IRQ_PVCCINF_CPU0_VRHOT_N) == GPIO_LOW)) {
+			sel_msg.event_type = IPMI_OEM_EVENT_TYPE_DEASSERT;
+		} else {
+			sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+		}
+
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_CPU_DIMM_VR_HOT;
+		sel_msg.sensor_number = SENSOR_NUM_VR_HOT;
+		sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_CPU_VR_HOT;
+		sel_msg.event_data2 = 0xFF;
+		sel_msg.event_data3 = 0xFF;
+		if (!common_add_sel_evt_record(&sel_msg)) {
+			LOG_ERR("Failed to add CPU VR HOT SEL\n");
+		}
+	}
+}
+
+void ISR_RMCA()
+{
+	if ((gpio_get(RST_PLTRST_BUF_N) == GPIO_LOW) || (gpio_get(PWRGD_CPU_LVC3) == GPIO_HIGH)) {
+		common_addsel_msg_t sel_msg;
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.sensor_type = IPMI_SENSOR_TYPE_PROCESSOR;
+		sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+		sel_msg.sensor_number = SENSOR_NUM_CATERR;
+		sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_MEM_RMCA;
+		sel_msg.event_data2 = 0xFF;
+		sel_msg.event_data3 = 0xFF;
+		if (!common_add_sel_evt_record(&sel_msg)) {
+			LOG_ERR("Failed to add RMCA SEL");
+		}
+	}
+}
+
+void ISR_CPU_VPP_INT()
+{
+	if (gpio_get(PWRGD_CPU_LVC3) == POWER_ON) {
+		int i = 0, ret = 0;
+		uint8_t retry = 3;
+		uint8_t vpp_pwr_status_bit = 0;
+		uint8_t device_id = 0;
+		uint8_t set_power_status = DEVICE_SET_POWER_OFF;
+		static uint8_t last_vpp_pwr_status =
+			0xE1; // default all devices are on (bit1~4 = 0)
+		I2C_MSG i2c_msg;
+		ipmi_msg msg;
+		common_addsel_msg_t sel_msg;
+
+		// Read VPP power status from SB CPLD
+		memset(&i2c_msg, 0, sizeof(I2C_MSG));
+		i2c_msg.bus = I2C_BUS1;
+		i2c_msg.target_addr = CPLD_ADDR;
+		i2c_msg.data[0] = CPLD_1OU_VPP_POWER_STATUS;
+		i2c_msg.tx_len = 1;
+		i2c_msg.rx_len = 1;
+		if (i2c_master_read(&i2c_msg, retry)) {
+			LOG_ERR("%s: Failed to read CPU VPP status, bus0x%x addr0x%x offset0x%x",
+				__func__, i2c_msg.bus, i2c_msg.target_addr, i2c_msg.data[0]);
+			return;
+		}
+
+		// Check BIOS is ready to handle VPP (post complete)
+		/*post complete pin hasn't been ready yet*/
+
+		for (device_id = 0; device_id < MAX_1OU_M2_COUNT; device_id++) {
+			// Shift to skip bit 0
+			// VPP power status is start on bit 1
+			vpp_pwr_status_bit = (i2c_msg.data[0] >> (device_id + 1)) & 0x1;
+			if (vpp_pwr_status_bit ==
+			    ((last_vpp_pwr_status >> (device_id + 1)) & 0x1)) {
+				continue;
+			}
+
+			if (vpp_pwr_status_bit == 0) {
+				set_power_status = DEVICE_SET_POWER_ON;
+			} else {
+				set_power_status = DEVICE_SET_POWER_OFF;
+			}
+
+			// Check 1OU E1.S power status before control
+			// If power status isn't changed, control the power
+			ret = get_set_1ou_m2_power(&msg, device_id, DEVICE_GET_POWER_STATUS);
+			if ((msg.data[3] != set_power_status) || (ret < 0)) {
+				// Notify 1OU BIC to turn on/off E1.S power
+				for (i = 0; i < retry; i++) {
+					ret = get_set_1ou_m2_power(&msg, device_id,
+								   set_power_status);
+					if (ret == 0) {
+						break;
+					}
+				}
+
+				if (i == retry) {
+					LOG_ERR("Failed to send OEM_1S_GET_SET_M2 command 0x%x to 0x%x device%x",
+						CMD_OEM_1S_GET_SET_M2, EXP1_IPMB,
+						_1ou_m2_name_mapping_table[device_id]);
+					continue;
+				}
+			}
+
+			// Add SEL about VPP power event
+			if (gpio_get(PWRGD_CPU_LVC3) == LOW_INACTIVE) {
+				memset(&sel_msg, 0, sizeof(common_addsel_msg_t));
+				sel_msg.InF_target = BMC_IPMB;
+				sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_SYS_STA;
+				sel_msg.event_type = IPMI_OEM_EVENT_TYPE_NOTIFY;
+				sel_msg.sensor_number = SENSOR_NUM_SYSTEM_STATUS;
+				sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_VPP_EVENT;
+				sel_msg.event_data2 = IPMI_OEM_EVENT_OFFSET_1OU;
+				sel_msg.event_data3 = _1ou_m2_name_mapping_table[device_id];
+				if (!common_add_sel_evt_record(&sel_msg)) {
+					LOG_ERR("Failed to initialize CPU VPP");
+				}
+			}
+		}
+		last_vpp_pwr_status = i2c_msg.data[0];
+	}
+}
