aaa09da02c131b155f19769e8b994da941c5b881
Eli_Huang <Eli_Huang@wiwynn.com>
2023-03-14 16:59:58 +0800

    bic to bic test

--- common/hal/hal_i2c.c
+++ common/hal/hal_i2c.c
@@ -117,8 +117,8 @@ int i2c_master_read(I2C_MSG *msg, uint8_t retry)
 		}
 	}
 
-	if (i > retry)
-		LOG_ERR("I2C %d master read retry reach max with ret %d", msg->bus, ret);
+	//if (i > retry)
+		//LOG_ERR("I2C %d master read retry reach max with ret %d", msg->bus, ret);
 
 exit:
 	SAFE_FREE(txbuf);
