bd27f148b8eae1363350cac40701f67318423bac
marshallZhan <Marshall_Zhan@wiwynn.com>
2023-06-05 12:08:52 +0800

    Artemis: mc: Version commit for obat-mc-2023.22.02 Description: - Version commit for MooseCreek BIC obat-mc-2023.22.02.
    Test Plan:
    - Build code: Pass
    - Get BIC version: Pass
    
    Log:
    root@bmc-oob:~# fw-util mc --version bic
    MC BIC Version: mc2023.22.02

--- meta-facebook/at-mc/src/platform/plat_version.h
+++ meta-facebook/at-mc/src/platform/plat_version.h
@@ -43,7 +43,7 @@
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x23
 #define BIC_FW_WEEK 0x22
-#define BIC_FW_VER 0x01
+#define BIC_FW_VER 0x02
 #define BIC_FW_platform_0 0x6d // char: m
 #define BIC_FW_platform_1 0x63 // char: c
 #define BIC_FW_platform_2 0x00 // char: '\0'
