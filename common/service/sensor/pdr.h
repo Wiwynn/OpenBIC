d0b8eec68157a678e101193df06a6be71c85c03d
PeterHo-wiwynn <Peter_MH_Ho@wiwynn.com>
2023-08-22 16:53:48 +0800

    common: service: support PLDM get PDR handler
    Summary:
    Description
    Implement pldm get pdr info and get pdr handler.
    
    Motivation
    For implementing sensor monitoring, the BMC should get sensor information from the BIC.
    
    Test Plan:
    - Build code: Pass
    - Add some PDR samples on platform and BMC get pdr from BIC: Pass
    
    Log:
    Get PDR info:
    pldmtool raw -m 8 -d 0x80 0x02 0x50
    pldmtool: Tx: 80 02 50
    pldmtool: Rx: 00 02 50 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 03 00 00 00 44 01 00 00 6c 00 00 00 00
    
    Get PDR:
    pldmtool platform GetPDR -m 8 -a -v
    [
    pldmtool: Tx: 80 02 51 00 00 00 00 00 00 00 00 01 ff ff 00 00
    Success in creating the socket : RC = 4
    Write to socket successful : RC = 16
    Total length:123
    pldmtool: Rx: 00 02 51 00 01 00 00 00 00 00 00 00 05 6c 00 00 00 00 00 01 02 00 00 60 00 00 00 00 10 20 00 01 88 00 10 00
    00 00 00 05 00 00 00 00 00 00 00 00 00 04 00 00 00 00 00 00 00 00 00 00 00 00 00 00 3f 00 00 00 00 00 00 00 00 00 00 00 00
    00 00 00 00 00 00 04 7f 00 00 00 00 00 00 00 00 00 00 00 00 00 00 0d 00 90 08 0a 00 00 20 0a 00 a0 0d 0a 00 b0 1b 0e 00 4d
    01 0a 00 5b 00
    {
        "nextRecordHandle": 1,
        "responseCount": 108,
        "recordHandle": 0,
        "PDRHeaderVersion": 1,
        "PDRType": "Numeric Sensor PDR",
        "recordChangeNumber": 0,
        "dataLength": 96
    }
    ,pldmtool: Tx: 81 02 51 01 00 00 00 00 00 00 00 01 ff ff 00 00
    Success in creating the socket : RC = 4
    Write to socket successful : RC = 16
    Total length:123
    pldmtool: Rx: 01 02 51 00 02 00 00 00 00 00 00 00 05 6c 00 01 00 00 00 01 02 00 00 60 00 00 00 00 10 2d 00 01 88 00 10 00
    00 00 00 05 00 00 00 00 00 00 00 00 00 04 00 00 00 00 00 00 00 00 00 00 00 00 00 00 06 00 00 00 00 00 00 00 00 00 00 00 00
    00 00 00 00 00 00 04 7f 00 00 00 00 00 00 00 00 00 00 00 00 00 00 6e 03 01 00 ca 02 01 00 80 03 01 00 89 02 01 00 02 00 02
    00 04 00 00 00
    {
        "nextRecordHandle": 2,
        "responseCount": 108,
        "recordHandle": 1,
        "PDRHeaderVersion": 1,
        "PDRType": "Numeric Sensor PDR",
        "recordChangeNumber": 0,
        "dataLength": 96
    }
    ,pldmtool: Tx: 82 02 51 02 00 00 00 00 00 00 00 01 ff ff 00 00
    Success in creating the socket : RC = 4
    Write to socket successful : RC = 16
    Total length:123
    pldmtool: Rx: 02 02 51 00 00 00 00 00 00 00 00 00 05 6c 00 02 00 00 00 01 02 00 00 60 00 00 00 00 10 01 00 01 88 00 10 00
    00 00 00 02 00 00 00 00 00 00 00 00 00 04 00 00 00 00 00 00 00 00 00 00 00 00 00 00 06 00 00 00 00 00 00 00 00 00 00 00 00
    00 00 00 00 00 00 04 29 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 34 00 00 00 96
    00 00 00 00 00
    {
        "nextRecordHandle": 0,
        "responseCount": 108,
        "recordHandle": 2,
        "PDRHeaderVersion": 1,
        "PDRType": "Numeric Sensor PDR",
        "recordChangeNumber": 0,
        "dataLength": 96
    }
    ]

---
+++ common/service/sensor/pdr.h
@@ -0,0 +1,87 @@
+#ifndef PDR_H
+#define PDR_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+extern uint32_t pdr_count;
+extern uint16_t pdr_config_size;
+uint8_t pdr_init(void);
+
+enum pdr_repository_state {
+	PDR_STATE_AVAILABLE = 0x00,
+	PDR_STATE_UPDATE_IN_PROGRESS = 0x01,
+	PDR_STATE_FAILED = 0x02,
+};
+
+typedef struct _PDR_common_header_ {
+	uint32_t record_handle;
+	uint8_t PDR_header_version;
+	uint8_t PDR_type;
+	uint16_t record_change_number;
+	uint16_t data_length;
+} PDR_common_header;
+
+typedef struct _PDR_numeric_sensor_ {
+	/*** PDR common header***/
+	PDR_common_header pdr_common_header;
+
+	/***numeric sensor format***/
+	uint16_t PLDM_terminus_handle;
+	uint16_t sensor_id;
+	uint16_t entity_type;
+	uint16_t entity_instance_number;
+	uint16_t container_id;
+	uint8_t sensor_init;
+	uint8_t sensor_auxiliary_names_pdr;
+	uint8_t base_unit;
+	int8_t unit_modifier;
+	uint8_t rate_unit;
+	uint8_t base_oem_unit_handle;
+	uint8_t aux_unit;
+	int8_t aux_unit_modifier;
+	uint8_t auxrate_unit;
+	uint8_t rel;
+	uint8_t aux_oem_unit_handle;
+	uint8_t is_linear;
+	uint8_t sensor_data_size;
+	int32_t resolution;
+	int32_t offset;
+	uint16_t accuracy;
+	uint8_t plus_tolerance;
+	uint8_t minus_tolerance;
+	uint8_t hysteresis;
+	uint8_t supported_thresholds;
+	uint8_t threshold_and_hysteresis_volatility;
+	int32_t state_transition_interval;
+	int32_t update_interval;
+	uint32_t max_readable;
+	uint32_t min_readable;
+	uint8_t range_field_format;
+	uint8_t range_field_support;
+	uint32_t nominal_value;
+	uint32_t normal_max;
+	uint32_t normal_min;
+	uint32_t warning_high;
+	uint32_t warning_low;
+	uint32_t critical_high;
+	uint32_t critical_low;
+	uint32_t fatal_high;
+	uint32_t fatal_low;
+} PDR_numeric_sensor;
+
+typedef struct _PDR_INFO_ {
+	uint8_t repository_state;
+	uint8_t update_time[13];
+	uint8_t oem_update_time[13];
+	uint32_t record_count;
+	uint32_t repository_size;
+	uint32_t largest_record_size;
+	uint8_t data_transfer_handle_timeout;
+} PDR_INFO;
+
+extern PDR_numeric_sensor *numeric_sensor_table;
+extern PDR_INFO *pdr_info;
+uint8_t plat_get_pdr_size();
+
+#endif
