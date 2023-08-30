4847e95e21b135cba1fae8671ef3fbda88a84a56
PeterHo-wiwynn <Peter_MH_Ho@wiwynn.com>
2023-08-30 09:47:14 +0800

    test

--- meta-facebook/yv4-ff/src/platform/plat_init.c
+++ meta-facebook/yv4-ff/src/platform/plat_init.c
@@ -15,8 +15,26 @@
  */
 
 #include "hal_gpio.h"
+#include "plat_mctp.h"
+#include "plat_i2c_target.h"
 
 #define DEF_PROJ_GPIO_PRIORITY 78
 
 DEVICE_DEFINE(PRE_DEF_PROJ_GPIO, "PRE_DEF_PROJ_GPIO_NAME", &gpio_init, NULL, NULL, NULL,
 	      POST_KERNEL, DEF_PROJ_GPIO_PRIORITY, NULL);
+
+void pal_pre_init()
+{
+	/* init i2c target */
+	for (int index = 0; index < MAX_TARGET_NUM; index++) {
+		if (I2C_TARGET_ENABLE_TABLE[index])
+			i2c_target_control(
+				index, (struct _i2c_target_config *)&I2C_TARGET_CONFIG_TABLE[index],
+				1);
+	}
+}
+
+void pal_post_init()
+{
+	plat_mctp_init();
+}
