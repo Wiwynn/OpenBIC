000077613f78014a90324c4513d08c010afb9a59
Jeff_Shih <Jeff_Shih@wiwynn.com>
2023-09-11 11:42:32 +0800

    version

--- meta-facebook/yv35-gl/src/platform/plat_version.h
+++ meta-facebook/yv35-gl/src/platform/plat_version.h
@@ -42,7 +42,7 @@
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x23
 #define BIC_FW_WEEK 0x35
-#define BIC_FW_VER 0x01
+#define BIC_FW_VER 0xe1
 #define BIC_FW_platform_0 0x67 // char: g
 #define BIC_FW_platform_1 0x6c // char: l
 #define BIC_FW_platform_2 0x00 // char: '\0'
