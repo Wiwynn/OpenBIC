7971a4e8ce44c914aa78f9cb9af2e48406f4dc78
Sara Lin <sara_sy_lin@wiwynn.com>
2023-10-17 16:26:14 +0800

    fby4: wf: Support CXL power sequence
    Description
    - Set GPIO setting, according to GPIO table "WAILUA FALLS_ASIC_BIC_GPIO table_20231006".
    - Support CXL power on/off sequence.
    - Enable the GPIO latch of the CXL power-related pin.
    
    Motivation
    - Support Vistara CXL power sequence.
    
    Test plan
    - Build code: Pass
    - Power on CXL: Pass
    - Power off CXL: Pass
    - Reset BIC: Pass
    
    Log
    1. Power on CXL.
    [00:00:00.037,000] <inf> plat_power_seq: MB DC (gpio num 7) status is ON
    [00:00:00.037,000] <wrn> power_status: DC_STATUS: off
    [00:00:00.040,000] <inf> usb_cdc_acm: Device suspended
    
    uart:~$ [00:00:00.563,000] <inf> plat_power_seq: CXL 1 power on success
    [00:00:01.089,000] <inf> plat_power_seq: CXL 2 power on success
    [00:00:01.089,000] <wrn> power_status: DC_STATUS: on
    [00:00:01.089,000] <wrn> power_status: DC_STATUS: on
    
    2. Power off CXL.
    uart:~$ [00:01:41.910,000] <inf> plat_power_seq: MB DC (gpio num 7) status is OFF
    [00:01:41.910,000] <wrn> power_status: DC_STATUS: off
    [00:01:41.910,000] <wrn> power_status: DC_STATUS: off
    uart:~$ [00:01:46.111,000] <inf> plat_power_seq: CXL 1 power off success
    [00:01:50.311,000] <inf> plat_power_seq: CXL 2 power off success
    
    3. Reset BIC to check CXL power.
    uart:~$ kernel reboot warm<)I00:00:00.002,000] <inf> usb_dc_asp*** Booting Zephyr OS build v00.01.06-54-g33f2715541ef  ***
    Hello, welcome to Yosemite V4 Wailua Falls 2023.36.1
    eed: select ep[0x81] as IN endpoint
    [00:00:00.002,000] <inf> usb_dc_aspeed: select ep[0x82] as IN endpoint
    [00:00:00.002,000] <wrn> usb_dc_aspeed: pre-selected ep[0x1] as IN endpoint
    [00:00:00.002,000] <wrn> usb_dc_aspeed: pre-selected ep[0x2] as IN endpoint
    [00:00:00.002,000] <inf> usb_dc_aspeed: select ep[0x3] as OUT endpoint
    [00:00:00.009,000] <err> spi_nor_multi_dev: [1347][spi1_cs0]SFDP magic 00000000 invalid
    [00:00:00.009,000] <err> spi_nor_multi_dev: [1609]SFDP read failed: -22
    [00:00:00.037,000] <err> sensor: Init sensor size is zero
    [00:00:00.037,000] <inf> plat_power_seq: MB DC (gpio num 7) status is ON
    [00:00:00.037,000] <wrn> power_status: DC_STATUS: on
    [00:00:00.037,000] <inf> plat_power_seq: MB DC (gpio num 7) status is ON
    [00:00:00.037,000] <inf> plat_power_seq: CXL DC status is ON
    [00:00:00.040,000] <inf> usb_cdc_acm: Device suspended
    
    uart:~$
    uart:~$ [00:00:05.038,000] <inf> util_sys: BIC Ready

--- meta-facebook/yv4-wf/src/platform/plat_isr.h
+++ meta-facebook/yv4-wf/src/platform/plat_isr.h
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -14,20 +14,10 @@
  * limitations under the License.
  */
 
-#include "hal_gpio.h"
-#include "util_sys.h"
-#include "power_status.h"
-#include "plat_gpio.h"
-#include "plat_power_seq.h"
+#ifndef PLAT_ISR_H
+#define PLAT_ISR_H
 
-#define DEF_PROJ_GPIO_PRIORITY 78
+void ISR_MB_DC_STAGUS_CHAGNE();
+void ISR_MB_PCIE_RST();
 
-void pal_set_sys_status()
-{
-	set_mb_dc_status(FM_POWER_EN_R);
-	set_DC_status(PG_CARD_OK);
-	set_sys_ready_pin(BIC_READY_R);
-}
-
-DEVICE_DEFINE(PRE_DEF_PROJ_GPIO, "PRE_DEF_PROJ_GPIO_NAME", &gpio_init, NULL, NULL, NULL,
-	      POST_KERNEL, DEF_PROJ_GPIO_PRIORITY, NULL);
+#endif
