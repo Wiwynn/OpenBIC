c936267e2b5fbf6bd531c62abc8b2efadf77d7eb
PeterHo-wiwynn <Peter_MH_Ho@wiwynn.com>
2023-10-20 16:05:10 +0800

    yv4-wf: add mctp routing table
    Description:
    Add mctp routing table for yv4-wf.
    
    Motivation:
    We need to support mctp over i2c/i3c for yv4.
    
    Test Plan:
    - Build code: Pass
    - Test on yv4 platform: Pass

--- meta-facebook/yv4-wf/src/platform/plat_init.c
+++ meta-facebook/yv4-wf/src/platform/plat_init.c
@@ -20,6 +20,8 @@
 #include "plat_gpio.h"
 #include "plat_power_seq.h"
 #include "plat_isr.h"
+#include "plat_mctp.h"
+#include "plat_i2c_target.h"
 
 #define DEF_PROJ_GPIO_PRIORITY 78
 
@@ -29,6 +31,22 @@ void pal_set_sys_status()
 	set_DC_status(PG_CARD_OK);
 	set_sys_ready_pin(BIC_READY_R);
 	set_ioe4_pin();
+}
+
+void pal_pre_init()
+{
+	/* init i2c target */
+	for (int index = 0; index < MAX_TARGET_NUM; index++) {
+		if (I2C_TARGET_ENABLE_TABLE[index])
+			i2c_target_control(
+				index, (struct _i2c_target_config *)&I2C_TARGET_CONFIG_TABLE[index],
+				1);
+	}
+}
+
+void pal_post_init()
+{
+	plat_mctp_init();
 }
 
 DEVICE_DEFINE(PRE_DEF_PROJ_GPIO, "PRE_DEF_PROJ_GPIO_NAME", &gpio_init, NULL, NULL, NULL,
