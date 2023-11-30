39764a145f56f297074e44342659f43455ebcaa3
Jeff_Shih <Jeff_Shih@wiwynn.com>
2023-11-30 13:46:16 +0800

    modify: stop get dimm info

--- meta-facebook/yv35-gl/src/platform/plat_init.c
+++ meta-facebook/yv35-gl/src/platform/plat_init.c
@@ -113,9 +113,9 @@ void pal_set_sys_status()
 
 void pal_device_init()
 {
-	init_i3c_dimm_prsnt_status();
-	start_get_dimm_info_thread();
-	start_monitor_pmic_error_thread();
+	//init_i3c_dimm_prsnt_status();
+	//start_get_dimm_info_thread();
+	//start_monitor_pmic_error_thread();
 	start_monitor_cpu_thread();
 }
 
