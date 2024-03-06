509a07dac20a0f749893daedda92f55ad6fd7470
MarshallZhan-wiwynn <Marshall_Zhan@wiwynn.com>
2024-03-06 16:53:03 +0800

    Common: Support to query device/downstream identifier Description: - Support PLDM command to query device/downstream identifier. - Add apply work function to allow each device to perform platform work in the apply_state. - Add component version string to compare firmware information. - Add function to set/get progress percent.
    Motivation:
    - Support PLDM command to query device/downstream identifier.
    - Add apply work function to allow each device to perform platform work in the apply_state.
    - Add component version string to compare firmware information.
    - Add function to set/get progress percent.
    - Initialize GT/YV4 project PLDM firmware update self_apply_work_func and comp_version_str parameter.
    
    Test Plan:
    - Build code: Pass in Artemis CB/MC, GT, YV4 FF/SD/WF platform
    - BIC PLDM firmware update: Pass

--- meta-facebook/yv4-sd/src/platform/plat_pldm_fw_update.c
+++ meta-facebook/yv4-sd/src/platform/plat_pldm_fw_update.c
@@ -79,6 +79,8 @@ pldm_fw_update_info_t PLDMUPDATE_FW_CONFIG_TABLE[] = {
 		.activate_method = COMP_ACT_SELF,
 		.self_act_func = pldm_bic_activate,
 		.get_fw_version_fn = NULL,
+		.self_apply_work_func = NULL,
+		.comp_version_str = NULL,
 	},
 	{
 		.enable = true,
@@ -92,6 +94,8 @@ pldm_fw_update_info_t PLDMUPDATE_FW_CONFIG_TABLE[] = {
 		.activate_method = COMP_ACT_AC_PWR_CYCLE,
 		.self_act_func = NULL,
 		.get_fw_version_fn = plat_get_vr_fw_version,
+		.self_apply_work_func = NULL,
+		.comp_version_str = NULL,
 	},
 	{
 		.enable = true,
@@ -105,6 +109,8 @@ pldm_fw_update_info_t PLDMUPDATE_FW_CONFIG_TABLE[] = {
 		.activate_method = COMP_ACT_AC_PWR_CYCLE,
 		.self_act_func = NULL,
 		.get_fw_version_fn = plat_get_vr_fw_version,
+		.self_apply_work_func = NULL,
+		.comp_version_str = NULL,
 	},
 	{
 		.enable = true,
@@ -118,6 +124,8 @@ pldm_fw_update_info_t PLDMUPDATE_FW_CONFIG_TABLE[] = {
 		.activate_method = COMP_ACT_AC_PWR_CYCLE,
 		.self_act_func = NULL,
 		.get_fw_version_fn = plat_get_vr_fw_version,
+		.self_apply_work_func = NULL,
+		.comp_version_str = NULL,
 	},
 	{
 		.enable = true,
@@ -131,6 +139,8 @@ pldm_fw_update_info_t PLDMUPDATE_FW_CONFIG_TABLE[] = {
 		.activate_method = COMP_ACT_SELF,
 		.self_act_func = NULL,
 		.get_fw_version_fn = plat_get_retimer_fw_version,
+		.self_apply_work_func = NULL,
+		.comp_version_str = NULL,
 	},
 	{
 		.enable = true,
@@ -144,6 +154,8 @@ pldm_fw_update_info_t PLDMUPDATE_FW_CONFIG_TABLE[] = {
 		.activate_method = COMP_ACT_SELF,
 		.self_act_func = NULL,
 		.get_fw_version_fn = plat_get_retimer_fw_version,
+		.self_apply_work_func = NULL,
+		.comp_version_str = NULL,
 	},
 };
 
