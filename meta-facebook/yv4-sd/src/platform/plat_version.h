c688c1e7ff2995ed1b8d06b06386566070652037
IngridChen-wiwynn <Ingrid_Chen@wiwynn.com>
2024-09-04 11:33:55 +0800

    yv4: sd: v2024.33.a2
    I3C transfter as default

--- meta-facebook/yv4-sd/src/platform/plat_version.h
+++ meta-facebook/yv4-sd/src/platform/plat_version.h
@@ -42,7 +42,7 @@
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x24
 #define BIC_FW_WEEK 0x33
-#define BIC_FW_VER 0xa1
+#define BIC_FW_VER 0xa2
 #define BIC_FW_platform_0 0x73 // char: s
 #define BIC_FW_platform_1 0x64 // char: d
 #define BIC_FW_platform_2 0x00 // char: '\0'
