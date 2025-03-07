fc8165a36f5c9e341f109ad7bf761485bc95130e
Yikai Tsai <yikai.tsai.wiwynn@gmail.com>
2025-03-07 11:15:22 +0800

    Revert "fby4: wf: Change CXL power sequence (#2233)"
    This reverts commit ec1ec07f9b39d1c536e736aaf67cc8581c7c0e9d.

--- meta-facebook/yv4-wf/src/platform/plat_isr.c
+++ meta-facebook/yv4-wf/src/platform/plat_isr.c
@@ -144,11 +144,8 @@ void ISR_MB_PCIE_RST()
 
 	// Monitor CXL ready and set CXL EID again
 	if (gpio_get(RST_PCIE_MB_EXP_N) == GPIO_HIGH) {
-		// If CXL didn't ready, check again
-		if ((!get_cxl_ready_status(CXL_ID_1)) || (!get_cxl_ready_status(CXL_ID_2))) {
-			create_check_cxl_ready_thread();
-			k_timer_start(&set_eid_timer, K_MSEC(25000), K_NO_WAIT);
-		}
+		create_check_cxl_ready_thread();
+		k_timer_start(&set_eid_timer, K_MSEC(10000), K_NO_WAIT);
 	} else {
 		// host reset, cxl also reset
 		set_cxl_ready_status(CXL_ID_1, false);
