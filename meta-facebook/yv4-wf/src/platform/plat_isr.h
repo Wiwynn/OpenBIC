c1dddc25084ff8f2f54048fa00d7edbd637350f9
LioraGuo-wiwynn <liora.guo.wiwynn@gmail.com>
2025-06-24 18:58:28 +0800

    fby4: wf: Enhance event logging for VR fault
    Summary:
    [Task Description]
    - Related to JIRA-1827
    - Implement different event logging format for PMBus and non-PMBus VRs
    - Extend support for additional WF VR power rails
    
    [Motivation]
    - Improve VR power fault reporting mechanism
    
    [Design]
    - Redesign the VR power fault detection and handling mechanism
      - Refactored the VR event handler to implement separate logging paths for PMBus and non-PMBus VRs.
    - Power Fault Detection Implementation
      - PMBus VRs : Utilize the related PWRGD pin and INT# from CPLD IOE1
      - non-PMBus VRs : Utilize the related PWRGD pin
    - Introduce new data structures
      - vr_fault_info: Describe PMBus VRs fault detection parameters

--- meta-facebook/yv4-wf/src/platform/plat_isr.h
+++ meta-facebook/yv4-wf/src/platform/plat_isr.h
@@ -59,22 +59,55 @@ typedef struct {
 
 typedef struct _add_vr_sel_info {
 	bool is_init;
-	uint8_t vr_num;
 	uint8_t vr_i2c_bus;
 	uint8_t vr_addr;
 	uint8_t page_cnt;
+	uint8_t gpio_num;
+	uint8_t vr_source;
 	struct k_work_delayable add_sel_work;
 } add_vr_sel_info;
+
+typedef struct {
+	uint8_t vr_source;
+	uint8_t ioe_reg_idx;
+	uint8_t ioe_bit;
+	uint8_t vr_pwrgd_gpio;
+	uint8_t vr_i2c_bus;
+	uint8_t vr_addr;
+	uint8_t vr_page;
+} vr_fault_info;
+
+typedef enum {
+	PMBUS_VR_IOE1_INT = 0,
+	NON_PMBUS_VR_PVTT_AB_ASIC1,
+	NON_PMBUS_VR_PVTT_AB_ASIC2,
+	NON_PMBUS_VR_PVTT_CD_ASIC1,
+	NON_PMBUS_VR_PVTT_CD_ASIC2,
+	NON_PMBUS_VR_PVPP_AB_ASIC1,
+	NON_PMBUS_VR_PVPP_AB_ASIC2,
+	NON_PMBUS_VR_PVPP_CD_ASIC1,
+	NON_PMBUS_VR_PVPP_CD_ASIC2,
+	NON_PMBUS_VR_P12V_E1S_0_R,
+	NON_PMBUS_VR_P3V3_E1S_0_R,
+} vr_event_index_t;
 
 void ISR_MB_DC_STAGUS_CHAGNE();
 void ISR_MB_PCIE_RST();
 void ISR_P3V3_E1S_PWR_CHANGE();
 void ISR_P12V_E1S_PWR_CHANGE();
 void ISR_CXL_PG_ON();
-void ISR_SET_CXL_LED();
-void ISR_IOE1_INT();
+void ISR_SET_CXL_LED_AND_CHECK_PWR_STATUS();
+void ISR_IOE1_INT_VR_FAULT();
+void ISR_PVTT_AB_ASIC1_PWR_VR_FAULT();
+void ISR_PVTT_AB_ASIC2_PWR_VR_FAULT();
+void ISR_PVPP_AB_ASIC1_PWR_VR_FAULT();
+void ISR_PVPP_AB_ASIC2_PWR_VR_FAULT();
+void ISR_PVPP_CD_ASIC1_PWR_VR_FAULT();
+void ISR_PVPP_CD_ASIC2_PWR_VR_FAULT();
 
 void init_vr_event_work();
-void process_vr_event_handler(struct k_work *work_item);
+void process_pmbus_vr_event_handler(struct k_work *work_item);
+void process_non_pmbus_vr_event_handler(struct k_work *work_item);
+void plat_isr_clear_vr_fault(uint8_t vr_addr, uint8_t vr_bus, uint8_t page);
 
 #endif
